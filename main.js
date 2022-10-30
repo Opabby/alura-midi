dc03b3d7ca65f887dc51cc7654374a497f72f168
Thaís Rodeiro <opabby82@gmail.com>
2022-10-29 21:16:57 -0300

    midi controler with 'while'

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0; 
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+while (contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    //'#som_{instrumento}' template string
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    contador++;
+}
